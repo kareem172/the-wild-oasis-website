f72fd21f2cd8d0de22ccf8d35c8eb2b1f319dc48
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-12 15:17:45 +0300

    Update layout to use grid for better responsiveness

--- app/layout.js
+++ app/layout.js
@@ -23,8 +23,8 @@ export default function RootLayout({ children }) {
         className={` ${josefinFont.className} antialiased bg-primary-950 min-h-svh text-primary-50 flex flex-col`}
       >
         <Header />
-        <div className=" flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto ">{children}</main>
+        <div className=" flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto  w-full">{children}</main>
         </div>
       </body>
     </html>

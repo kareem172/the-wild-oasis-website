ae6c51824ee4e9f602281fb40ecd8c170a0c5206
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-13 16:11:02 +0300

    feat: Update Cabins page to use lazy loading for images

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,14 +1,12 @@
-import CabinCard from "../_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import Spinner from "../_components/Spinner";
+import CabinsList from "../_components/CabinsList";
 
 export const metadata = {
   title: "Cabins",
 };
 
 export default async function Page() {
-  // CHANGE
-  const cabins = await getCabins();
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -18,18 +16,14 @@ export default async function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinsList />
+      </Suspense>
     </div>
   );
 }

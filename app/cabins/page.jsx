9488d881bf8a0dc67082e50ba7419c93eb010a94
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-22 02:31:56 +0300

    feat: Add CabinFilter component for filtering cabins by capacity

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
 import CabinsList from "../_components/CabinsList";
+import CabinFilter from "../_components/CabinFilter";
 
 export const revalidate = 3600; // 1 hour
 
@@ -8,7 +9,8 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,9 +24,11 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-
-      <Suspense fallback={<Spinner />}>
-        <CabinsList />
+      <div className=" flex justify-end pb-5">
+        <CabinFilter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinsList filter={filter} />
       </Suspense>
     </div>
   );

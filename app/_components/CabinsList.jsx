439ca29cae60dea7da7ae9cbaeb2cb3ef2418eb1
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-22 02:32:02 +0300

    feat: Filter cabins by capacity in CabinsList component

--- app/_components/CabinsList.jsx
+++ app/_components/CabinsList.jsx
@@ -1,13 +1,22 @@
 import CabinCard from "../_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
 
-async function CabinsList() {
+async function CabinsList({ filter }) {
   const cabins = await getCabins();
-
+  let filteredCabins = cabins;
+  if (filter !== "all") {
+    filteredCabins = cabins.filter((cabin) => {
+      if (filter === "small") return cabin.maxCapacity <= 3;
+      if (filter === "medium")
+        return cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7;
+      if (filter === "large")
+        return cabin.maxCapacity >= 8 && cabin.maxCapacity <= 12;
+    });
+  }
   if (!cabins.length) return null;
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {filteredCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>

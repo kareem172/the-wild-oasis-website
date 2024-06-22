54212c71031abef8addae791430d4cc6fbf7806c
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-22 04:05:07 +0300

    chore: Add ReservationProvider to layout for managing cabin reservations

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Header from "./_components/Header";
 import Logo from "./_components/Logo";
+import { ReservationProvider } from "./_components/ReservationContext";
 import Navigation from "./_components/navigation";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
@@ -24,7 +25,9 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <div className=" flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto  w-full">{children}</main>
+          <main className="max-w-7xl mx-auto  w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>

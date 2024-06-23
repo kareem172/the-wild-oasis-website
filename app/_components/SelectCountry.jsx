50ab976a2c07aa5364896404d78a70acf68dc71b
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-23 14:55:25 +0300

    refactor: Update flag retrieval logic in SelectCountry component

--- app/_components/SelectCountry.jsx
+++ app/_components/SelectCountry.jsx
@@ -5,8 +5,7 @@ import { getCountries } from '@/app/_lib/data-service';
 async function SelectCountry({ defaultCountry, name, id, className }) {
   const countries = await getCountries();
   const flag =
-    countries.find((country) => country.name.toLowerCase() === defaultCountry)?.flag ?? '';
-
+    countries.find((country) => country.name === defaultCountry)?.flag ?? '';
   return (
     <select
       name={name}

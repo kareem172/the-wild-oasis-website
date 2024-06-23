8b138c103279773687787cc9ba79105f5d5fff4a
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-23 15:01:28 +0300

    feat: Update updateGuest action to trigger revalidation of profile path cache

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,4 +1,5 @@
 "use server";
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 export async function updateGuest(formData) {
@@ -21,6 +22,8 @@ export async function updateGuest(formData) {
     .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
 }
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });

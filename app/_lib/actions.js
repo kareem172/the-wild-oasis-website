f311c365d29816ba619f473a443bfdf2ba3f1e16
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-23 14:55:03 +0300

    feat: Update guest information in updateGuest action

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,27 @@
 "use server";
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("Unauthorized");
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
 
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Invalid national ID");
+  const updatedData = {
+    nationalID,
+    nationality,
+    countryFlag,
+  };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updatedData)
+    .eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+}
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }

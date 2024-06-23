915d2bcaf2d356e3063fd7b5f386074648333a32
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-23 14:31:52 +0300

    add authentication and authorization

--- app/_components/Reservation.jsx
+++ app/_components/Reservation.jsx
@@ -1,16 +1,23 @@
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
 async function Reservation({ cabin }) {
-  const [setting, bookedDays] = await Promise.all([
+  const [setting, bookedDays, session] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
+    auth(),
   ]);
   return (
     <div className=" grid grid-cols-2 border border-primary-800 max-h-[400px] ">
       <DateSelector setting={setting} cabin={cabin} bookedDays={bookedDays} />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm user={session.user} cabin={cabin} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
--- app/_components/navigation.jsx
+++ app/_components/navigation.jsx
@@ -1,24 +1,40 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const { user } = (await auth()) || {};
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
         <li>
-          <Link href="/cabins" className="hover:text-accent-400 transition-colors">
+          <Link
+            href="/cabins"
+            className="hover:text-accent-400 transition-colors"
+          >
             Cabins
           </Link>
         </li>
         <li>
-          <Link href="/about" className="hover:text-accent-400 transition-colors">
+          <Link
+            href="/about"
+            className="hover:text-accent-400 transition-colors"
+          >
             About
           </Link>
         </li>
         <li>
           <Link
             href="/account"
-            className="hover:text-accent-400 transition-colors"
+            className="hover:text-accent-400 transition-colors flex items-center gap-2"
           >
+            {user && (
+              <img
+                src={user.image}
+                alt={user.name}
+                className="w-8 h-8 rounded-full"
+                referrerPolicy="no-referrer"
+              />
+            )}
             Guest area
           </Link>
         </li>

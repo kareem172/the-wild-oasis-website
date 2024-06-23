357363d620fc51a660857f740639a49cf8083d56
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-23 16:08:27 +0300

    feat: Fetch and display guest reservations on the reservations page

--- app/account/reservations/page.jsx
+++ app/account/reservations/page.jsx
@@ -1,11 +1,13 @@
 import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
 export const metadata = {
-  title: 'Reservations',
-}
+  title: "Reservations",
+};
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function Page() {
+  const { user } = await auth();
+  const bookings = await getBookings(user.guestId);
 
   return (
     <div>
